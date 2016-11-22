5e0a16ce2f1c30687d9ae410b37cad5290b140ba
ThieryMichel <thiery@marmelab.com>
2016-11-22 17:58:25 +0100

    add missing env variable for serverless-dev-server

--- config/default.js
+++ config/default.js
@@ -5,7 +5,7 @@ module.exports = {
             endpoint: 'http://dynamodb:8000',
             accessKeyId: process.env.AWS_ACCESS_KEY_ID,
             secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-            region: 'us-east-1',
+            region: 'eu-west-1',
         },
     },
 };
