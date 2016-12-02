577578c1a15a0da42e19e4602616b66d68c619ca
ThieryMichel <thiery@marmelab.com>
2016-12-02 16:58:37 +0100

    remove unused import

--- src/serverless/weeklyMessage.js
+++ src/serverless/weeklyMessage.js
@@ -1,9 +1,5 @@
-import sg, { call } from 'sg.js';
-import { batchSize } from 'config';
+import sg from 'sg.js';
 
-import smoker from './services/smoker';
-import getWeeklySmoker from './effects/getWeeklySmoker';
-import sendWeeklyMessage from './effects/sendWeeklyMessage';
 import weeklyMessageSaga from './weeklyMessage/saga';
 
 export default function dailyMessage(event, ctx, cb) {
