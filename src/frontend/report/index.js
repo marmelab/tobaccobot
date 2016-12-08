5d39caf0764a10f87f38ba6e9f0ead81717158da
ThieryMichel <thiery@marmelab.com>
2016-12-08 16:07:05 +0100

    split graph logic

--- src/frontend/report/index.js
+++ src/frontend/report/index.js
@@ -2,6 +2,7 @@ import * as d3 from 'd3';
 import fetch from 'isomorphic-fetch';
 
 const consumptionHistory = [
+    40,
     30, 38, 36, 34, 34, 32, 30,
     28, 26, 24, 22, 20, 22, 20,
     18, 16, 16, 14, 12, 8, 10,
@@ -9,6 +10,71 @@ const consumptionHistory = [
 ];
 
 const targetConsumptionHistory = [30, 20, 10, 0];
+
+function renderXaxis(svg, xScale, height, margin) {
+    svg.append('g')
+    .attr('transform', `translate(${margin}, ${height - margin})`)
+    .call(d3.axisBottom(xScale).tickFormat(v => `d${v}`).tickValues([0, 7, 14, 21, 28]))
+    .style('font-size', '24px');
+}
+
+function renderYAxis(svg, yScale, margin) {
+    svg.append('g')
+    .attr('transform', `translate(${margin}, ${margin})`)
+    .call(d3.axisLeft(yScale))
+    .style('font-size', '24px');
+}
+
+function renderTargetGraph(svg, xScale, yScale, height, margin) {
+    const targetLine = d3
+    .line()
+    .curve(d3.curveStepAfter)
+    .x((d, i) => margin + xScale(i * 7))
+    .y(d => margin + yScale(d));
+
+    const targetArea = d3
+    .area()
+    .curve(d3.curveStepAfter)
+    .x((d, i) => margin + xScale(i * 7))
+    .y(d => margin + yScale(d))
+    .y0(height - margin)
+    .y1(d => yScale(d) + margin);
+
+    svg.append('path')
+    .datum(targetConsumptionHistory)
+    .attr('d', targetArea)
+    .attr('fill', '#aaffaa');
+
+    svg.append('path')
+    .datum(targetConsumptionHistory)
+    .style('stroke', 'black')
+    .attr('d', targetLine)
+    .attr('fill', 'none');
+}
+
+function rendetConsumptionGraph(svg, xScale, yScale, margin) {
+    const consumptionLine = d3
+    .line()
+    .x((d, i) => margin + xScale(i))
+    .y(d => margin + yScale(d));
+
+    svg
+    .append('path')
+    .datum(consumptionHistory)
+    .attr('d', consumptionLine)
+    .style('stroke', 'black')
+    .attr('fill', 'none');
+
+    svg
+    .append('g')
+    .selectAll('circle')
+    .data(consumptionHistory)
+    .enter()
+    .append('circle')
+    .attr('r', 5)
+    .attr('cx', (_, cx) => margin + xScale(cx))
+    .attr('cy', cy => margin + yScale(cy));
+}
 
 function render() {
     d3.select('svg').remove();
@@ -22,71 +88,18 @@ function render() {
 
     const x = d3.scaleLinear()
     .range([0, width - (margin * 2)])
-    .domain([1, targetConsumptionHistory.length * 7]);
-
-    const targetLine = d3
-    .line()
-    .curve(d3.curveStepAfter)
-    .x((d, i) => margin + x((i * 7) + 1))
-    .y(d => margin + y(d));
-
-    const targetArea = d3
-    .area()
-    .curve(d3.curveStepAfter)
-    .x((d, i) => margin + x((i * 7) + 1))
-    .y(d => margin + y(d))
-    .y0(height - margin)
-    .y1(d => y(d) + margin);
+    .domain([0, targetConsumptionHistory.length * 7]);
 
     const svg = d3.select('#report')
     .append('svg')
     .attr('width', width)
     .attr('height', height);
 
-    svg.append('path')
-    .datum(targetConsumptionHistory)
-    .attr('d', targetArea)
-    .attr('fill', '#aaffaa');
+    renderTargetGraph(svg, x, y, height, margin);
+    rendetConsumptionGraph(svg, x, y, margin);
+    renderXaxis(svg, x, height, margin);
 
-    const targetGraph = svg.append('path')
-    .datum(targetConsumptionHistory)
-    .style('stroke', 'black')
-    .attr('d', targetLine)
-    .attr('fill', 'none');
-
-    const consumptionGraph = svg
-    .append('g')
-    .selectAll('circle')
-    .data(consumptionHistory)
-    .enter()
-    .append('circle')
-    .attr('r', 10)
-    .attr('cx', (_, cx) => margin + x(cx + 1))
-    .attr('cy', cy => margin + y(cy));
-
-    // Add the x Axis
-    svg.append('g')
-    .attr('transform', `translate(${margin}, ${height - margin})`)
-    .call(d3.axisBottom(x));
-
-    // text label for the x axis
-    svg.append('text')
-    .attr('transform', `translate(${width / 2}, ${height - 10})`)
-    .style('text-anchor', 'middle')
-    .text('Days');
-
-    // Add the y Axis
-    svg.append('g')
-    .attr('transform', `translate(${margin}, ${margin})`)
-    .call(d3.axisLeft(y));
-
-    svg.append('text')
-    .attr('transform', 'rotate(-90)')
-    .attr('y', 0)
-    .attr('x', 0 - (height / 2))
-    .attr('dy', '1em')
-    .style('text-anchor', 'middle')
-    .text('Cigarettes');
+    renderYAxis(svg, y, margin);
 }
 
 d3.select(window).on('resize', render);
