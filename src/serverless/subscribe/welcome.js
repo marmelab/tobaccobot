3eac676dcb69ea748f43231544df8d9ffd56ffe9
djhi <gildas.garcia@gmail.com>
2016-12-12 11:28:47 +0100

    Added instructions in first message

--- src/serverless/subscribe/welcome.js
+++ src/serverless/subscribe/welcome.js
@@ -1,5 +1,6 @@
 export default name => (
 `Hi ${name}, and thanks for joining the tobaccobot program!
+You can stop the program anytime by sending STOP to this number.
 During the next 4 weeks, I'll text you every morning to ask about your cigarette consumption of the day before.
 Just reply with the number and I'll do my best to help you quit smoking!
 And to begin with, how many cigarettes did you smoke yesterday ?`
