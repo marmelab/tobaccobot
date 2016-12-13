ec6f33cb07a1d21dda055d946ed58394261b81f7
djhi <gildas.garcia@gmail.com>
2016-12-13 12:25:32 +0100

    Remove report links from end of program messages

--- src/serverless/dailyMessage/newWeek/end/sendFailureMessage.js
+++ src/serverless/dailyMessage/newWeek/end/sendFailureMessage.js
@@ -1,11 +1,9 @@
-import config from 'config';
 import sendSms from '../../../services/sendSms';
 
 export const failureMessage = () => (
 `Ok, I've done my best for the past 28 days, but it's not only in my hands -
 YOU are the one who can stop smoking.
 We'll stop this for now, but I'm confident that you can try another time.
-You can see a chart about your progress at: {ch1}.
 We'll keep in touch!`
 );
 
@@ -13,7 +11,7 @@ export const sendFailureMessageFactory = send => (phones) => {
     if (!phones || !phones.length) {
         return;
     }
-    send(phones, failureMessage(), phones.map(phone => `${config.reportLink}?phone=${encodeURIComponent(phone)}`));
+    send(phones, failureMessage());
 };
 
 export default sendFailureMessageFactory(sendSms);
--- src/serverless/dailyMessage/newWeek/end/sendSuccessMessage.js
+++ src/serverless/dailyMessage/newWeek/end/sendSuccessMessage.js
@@ -1,4 +1,3 @@
-import config from 'config';
 import sendSms from '../../../services/sendSms';
 
 export const successMessage = () => (
@@ -6,7 +5,6 @@ export const successMessage = () => (
 I think we can agree that you have succeeded.
 Congratulations, I'm proud of you!
 And from now on, you're on your own - I won't bother you again.
-You can see a chart about your progress at: {ch1}.
 It's been a pleasure to coach you! Cheers.`
 );
 
@@ -14,7 +12,7 @@ export const sendSuccessMessageFactory = send => (phones) => {
     if (!phones || !phones.length) {
         return;
     }
-    send(phones, successMessage(), phones.map(phone => `${config.reportLink}?phone=${encodeURIComponent(phone)}`));
+    send(phones, successMessage());
 };
 
 export default sendSuccessMessageFactory(sendSms);
